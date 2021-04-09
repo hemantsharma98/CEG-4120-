6d316eeb5d03b643a740220c2b7d50689dd14ff5
swallow11wrightedu <43588596+swallow11wrightedu@users.noreply.github.com>
2021-04-09 14:07:05 -0400

    Update monoCode.cpp

--- monoCode/monoCode.cpp
+++ monoCode/monoCode.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Property.h"
 #include "Inventory.h"
+#include <random>
 using namespace std;
 
 void buildBoard(Property board[40]) {
@@ -34,20 +35,41 @@ class Player {
 public:
     Inventory inv = Inventory();
     Property owned[30];
+    
     Player() {
-        inv.setpID = 1;
-        inv.addMoney = 2500;
-        inv.setLoc = 0;
+        inv.setLoc(0);
+        inv.setpID(1);
+        inv.addMoney(2500);
     }
 };
+
+int generateOneThruSix() {
+    random_device rd; //Obtain random number from hardware
+    mt19937 gen(rd()); // seed the generator
+    uniform_int_distribution<> distr(1, 6); // define the range
+
+    return distr(gen);
+}
+
+bool isDouble(int d1, int d2) {
+    return d1 == d2;
+}
+
+void movePlayer(Player p) {
+    int d1 = generateOneThruSix();
+    int d2 = generateOneThruSix();
+    cout << d1 << " " << d2 << endl;
+    cout << p.inv.getLoc() << endl;
+    p.inv.setLoc(d1 + d2);
+    cout << p.inv.getLoc() << endl;
+}
 
 int main()
 {
     Property board[40];
     buildBoard(board);
-    cout << board[0].getFourHouse() << endl;
-    cout << board[2].getPropName() << endl;
 
     Player player1 = Player();
 
+    movePlayer(player1);
 }
